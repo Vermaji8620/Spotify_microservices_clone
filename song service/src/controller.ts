f86f9a7c6c846d989ddb7e27fb165fa1ee2f26ce
Vermaji8620 <rajaditya14032002@gmail.com>
2026-04-30 23:41:39 +0530

    feat: integrate Redis caching for album and song retrieval in controller

--- song service/src/controller.ts
+++ song service/src/controller.ts
@@ -1,33 +1,78 @@
 import { sql } from './config/db.js';
+import { redisClient } from './index.js';
 import trycatch from './trycatch.js';
 
 
 export const getAllAlbums = trycatch(
     async (req, res) => {
         let albums;
+        const cache_expiry = 1800
 
-        albums = await sql`
-            SELECT * FROM albums
-        `
-        res.status(200).json(albums)
+        if (redisClient.isReady) {
+            albums = await redisClient.get('albums')
+        }
+        if (albums) {
+            console.log("cache hit")
+            res.status(200).json(JSON.parse(albums))
+            return;
+        }
+        else {
+            console.log("cache miss")
+            albums = await sql`
+                SELECT * FROM albums
+            `
+            if (redisClient.isReady) {
+                await redisClient.set('albums', JSON.stringify(albums), {
+                    EX: cache_expiry
+                })
+            }
+            res.status(200).json(albums)
+        }
     }
 )
 
 export const getAllSongs = trycatch(
     async (req, res) => {
+        const cache_expiry = 1800;
         let songs;
-
-        songs = await sql`
-            SELECT * FROM songs
-        `
-        res.status(200).json(songs)
+        if (redisClient.isReady) {
+            songs = await redisClient.get('songs')
+        }
+        if (songs) {
+            console.log("cache hit")
+            res.status(200).json(JSON.parse(songs))
+            return;
+        }
+        else {
+            console.log("cache miss")
+            songs = await sql`
+                SELECT * FROM songs
+            `
+            if (redisClient.isReady) {
+                await redisClient.set('songs', JSON.stringify(songs), {
+                    EX: cache_expiry
+                })
+            }
+            res.status(200).json(songs)
+        }
     }
 )
 
 export const getAllSongsOfAlbum = trycatch(
     async (req, res) => {
+        const cache_expiry = 1800;
         const album_id = req.params.id;
-        const album_id_exists = await sql`
+        let album_id_exists;
+        if (redisClient.isReady) {
+            album_id_exists = await redisClient.get(`album:${album_id}`);
+        }
+        if (album_id_exists) {
+            console.log("Cache hit")
+            res.status(200).json(JSON.parse(album_id_exists))
+            return;
+        }
+        console.log("Cache miss")
+        album_id_exists = await sql`
             SELECT * FROM albums WHERE id = ${album_id}
         `
         if (!album_id_exists || album_id_exists.length == 0) {
@@ -35,10 +80,15 @@ export const getAllSongsOfAlbum = trycatch(
             return;
         }
 
-        const allSongs = await sql`
-            SELECT * FROM songs WHERE album_id = ${album_id}
-        `
-
+        let allSongs;
+        allSongs = await sql`
+                SELECT * FROM songs WHERE album_id = ${album_id}
+            `
+        if (redisClient.isReady) {
+            await redisClient.set(`album:${album_id}`, JSON.stringify({
+                songs: allSongs, album: album_id_exists[0]
+            }), { EX: cache_expiry })
+        }
         res.status(200).json({ songs: allSongs, album: album_id_exists[0] })
     }
 )
--- song service/src/index.ts
+++ song service/src/index.ts
@@ -1,6 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
+import redis from "redis"
 import songRoutes from "./route.js"
+
+
+export const redisClient = redis.createClient({
+    password: process.env.Redis_Password as string,
+    socket: {
+        host: "redis-19891.crce179.ap-south-1-1.ec2.cloud.redislabs.com",
+        port: 19891
+    }
+})
+
+redisClient.connect().then(() => {
+    console.log("Redis connected")
+}).catch((error) => { console.log(error) })
 
 const app = express()
 dotenv.config()
